766a6dc1164f79ff36dba7c973a533cfc8ea07c1
Ehar Kala <eharkala@ut.ee>
2021-12-14 02:04:27 +0200

    finished

--- main.py
+++ main.py
@@ -1,45 +1,77 @@
-from os import name
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 from pyproj import Proj, transform
 import plotly.graph_objects as go
 from math import sqrt
 
-df = pd.read_csv("lidarFiles/cameraDriving_back_frames.csv", skipinitialspace=True, usecols= ['autonomous', 'steering_angle', 'vehicle_speed', 'position_x', 'position_y', 'position_z', 'roll', 'pitch', 'yaw'])
+df = pd.read_csv("lidarFiles/cameraDriving_froward_frames.csv", skipinitialspace=True, usecols= ['autonomous', 'steering_angle', 'vehicle_speed', 'position_x', 'position_y', 'position_z', 'roll', 'pitch', 'yaw'])
 
+
+# ------------------- DRIVING WHITENESS ---------------------
 def driving_whiteness(df):
-    sum=0
     whitenessValues=[]
-    D = df.shape[0]
+    D = df.shape[0]  
     for i in range(D):
-        if i < 2:
-            sum += abs(df["steering_angle"][i]/0.1)
-        elif i > D-3:
-            sum += abs(df["steering_angle"][i]/0.1)
-        else:
-            sum = abs((df["steering_angle"][i]-df["steering_angle"][i-1])/0.1)
+        sum=0
+        keskmisteArv=0
+        for j in range(i-20, i+21):
+            if not j>D-1 and not j<1:
+                keskmisteArv+=1
+                sum += abs(df["steering_angle"][j]-df["steering_angle"][j-1])
+        sum = sum/keskmisteArv
         whitenessValues.append(sqrt((1/D)*sum))
     return whitenessValues
+# -----------------------------------------------------------
 
-# DATA PREPARATION
-df['autonomous'] = df['autonomous'].replace([True, False], [1, 0])
+
+
+# ------------------- DATA PREPARATION ----------------------
+
+# converts true/false values to numeric values (0 and 1) to
+# use it as a size scale when creating a trace on the map
+df.autonomous = df.autonomous.replace([True, False], [1, 0])
+
+# calculates driving whiteness with previously defined function
+# and assigns returned values to a dataframe column
 df = df.assign(driving_whiteness=driving_whiteness(df))
 
-def cordsToWSG84(df):
-    estcords = Proj(init='epsg:3301')
-    worldcords = Proj(init='epsg:4326')
-    lon, lat = transform(estcords, worldcords, df.position_x+650000, df.position_y+6465000)
-    return (lat, lon)
+# converts m/s -> km/h
+df.vehicle_speed = df.vehicle_speed*60*60/1000
 
+# -----------------------------------------------------------
+
+
+
+# ------------------ COORDINATE SYSTEM ----------------------
+def cordsToWSG84(df):
+    estcords = Proj(init='epsg:3301')       # Estonian Coordinate System of 1997
+    worldcords = Proj(init='epsg:4326')     # World Geodetic System 1984
+
+    # actual conversion with pyproj -->
+    longitude, latitude = transform(estcords, worldcords, df.position_x+650000, df.position_y+6465000)
+
+    return (latitude, longitude)
+# -----------------------------------------------------------
+
+
+
+# ----------- GENERIC PLOT-ON-MAP FUNCTION ------------------
 def plotByColor(df, color, title="Driving trajectory"):
+
+    # converts epsg:3301 from csv to epsg:4326 WSG84 coordinate system -->
     lat, lon = cordsToWSG84(df)
 
-    fig = px.scatter_mapbox(df, lat=lat, lon=lon, size_max=5, zoom=12.5, height=800, width=1900, color=color,
+    # generates main trace on map that represents the main information of the 
+    # plot (speed/height/whiteness of the driving) based on the functions "color" 
+    # variable value (one of the dataframe column names)
+    fig = px.scatter_mapbox(df, lat=lat, lon=lon, size_max=5, zoom=12.5, height=960, width=1800, color=color,
         hover_data={'autonomous': True, 'position_x': False, 'position_y': False, 'position_z': False, 'roll': False, 'pitch': False, 'yaw': False}, 
-        title=title, opacity=0.4)
+        title=title)
     
+    # generates the trace of autonomous drivin on the map which exists 
+    # when the car is driving by itself but disappears when driving
+    # is taken over by the driver
     fig.add_trace(go.Scattermapbox(
          lat=lat,
          lon=lon,
@@ -48,13 +80,21 @@ def plotByColor(df, color, title="Driving trajectory"):
             size=df["autonomous"].tolist(),
             color='rgb(255,255,255)',
             sizemin=1.3,
+            opacity=0.8
          ),
-         name="  TOGGLE -> Autonomous driving"
+         # spaces necessary to get rid of overlaping titles -->
+         name="                                       TOGGLE -> Autonomous driving"
      ))
-
     fig.update_layout(mapbox_style="open-street-map")
     fig.show()
+# -----------------------------------------------------------
 
-#plotByColor(df, "vehicle_speed", "Trajectory")
-#plotByColor(df, "position_z", "Trajectory")
+# FOLLOWING TRACES ARE ALL COMBINED WITH AUTONOMOUS 
+# DRIVING TRACE (white) THAT CAN BE TOGGLED
+
+# generates trace showcasing vehicle speed
+plotByColor(df, "vehicle_speed", "Trajectory")
+# generates trace showcasing the height of driving from sea-level
+plotByColor(df, "position_z", "Trajectory")
+# generates trace showcasing driving whiteness
 plotByColor(df, 'driving_whiteness', "Trajectory")
